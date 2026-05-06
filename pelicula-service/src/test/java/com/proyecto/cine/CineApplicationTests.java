d3334bbdeca4b5b43307cb866921f78e654b78d4
Gabo70-1 <ga.yanezg@duocuc.cl>
2026-05-06 09:26:59 -0400

    Remove unnecessary line and fix formatting

--- pelicula-service/src/test/java/com/proyecto/cine/CineApplicationTests.java
+++ pelicula-service/src/test/java/com/proyecto/cine/CineApplicationTests.java
@@ -2,7 +2,7 @@ package com.proyecto.cine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+asdasdasdasdasdasdasda;
 @SpringBootTest
 class CineApplicationTests {
 
